471ec91ac560547deeaccbd0eafc8250a0029116
gumanist29 <43199897+gumanist29@users.noreply.github.com>
2019-03-08 13:35:37 +0600

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-import fractions
-
-def lcm(n):
-	""" Return smallest positive number that is evenly divisible by all of the numbers from 1 to 20"""
-    ans = 1    
-    for i in range(1, n + 1): 
-        ans = (ans * i)/fractions.gcd(ans, i)         
-    return ans 
-print(lcm(20))
